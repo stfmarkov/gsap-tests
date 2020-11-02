74f1a2035f25df5cc565bf610ad199421e54edb6
stefankostov <stefankostov91@gmail.com>
2020-11-02 19:58:42 +0200

    test

--- animationSections/5GSection.js
+++ animationSections/5GSection.js
@@ -1,11 +1,11 @@
 function init5GSection() {
 
-  const animationLength = 2500; // Set the time to complete the animation
+  const animationLength = 1700; // Set the time to complete the animation
 
   // pin the section
   ScrollTrigger.create({
     trigger: ".test-text",
-    start: "top top",
+    start: "top top",// when the top of the trigger hits the top of the viewport
     end: `+=${animationLength}px`,
     pin: true,
   });
@@ -13,8 +13,7 @@ function init5GSection() {
   let tl_v = gsap.timeline({
     scrollTrigger: {
       trigger: ".first-screen",
-      start: "top 80%", // when the top of the trigger hits the top of the viewport
-      end: `+=${animationLength}px`,
+      start: "top top",
       scrub: true,
       pinspacer: false,
     },
@@ -25,6 +24,7 @@ function init5GSection() {
       .from(".center", {
         scaleX: 3.7,
         scaleY: 3.7,
+          y: "-350px",
         duration: 1,
       },
           "start"
@@ -37,6 +37,7 @@ function init5GSection() {
       },
           "start+=0.05"
           )
+      .addLabel("right")
       .from(".right", {
         scaleX: 4.5,
         scaleY: 4.5,
@@ -45,24 +46,31 @@ function init5GSection() {
       },
           "start+=0.05"
           )
+      .from(".logo-sign", {
+              opacity: 0,
+              scaleX: 1.5,
+              scaleY: 1.5,
+              duration: 1,
+          }
+      )
 
-  
 
-  gsap.fromTo(
-      ".test-text",
-      {
-        translateY: 0,
-      },
-      {
-        translateY: 170,
-        scrollTrigger: {
-          trigger: ".phone",
-          scrub: true,
-          start: "top 80%",
-          ease: Power4.easeOut,
-        },
-      }
-  )
+
+  // gsap.fromTo(
+  //     ".test-text",
+  //     {
+  //       translateY: 0,
+  //     },
+  //     {
+  //       translateY: 170,
+  //       scrollTrigger: {
+  //         trigger: ".phone",
+  //         scrub: true,
+  //         start: "top 80%",
+  //         ease: Power4.easeOut,
+  //       },
+  //     }
+  // )
 
   gsap.fromTo(
       ".move-text",
@@ -78,125 +86,126 @@ function init5GSection() {
           end: "top -50%",
           ease: Power4.easeOut,
         },
-      }
+      },
+      "start-=2"
   );
 
-  gsap.fromTo(
-      ".phone",
-      {
-        translateY: 70,
-      },
-      {
-        translateY: -150,
-        scrollTrigger: {
-          trigger: ".phone",
-          scrub: true,
-          start: "top 80%",
-          ease: Power4.easeOut,
-        },
-      }
-  );
+  // gsap.fromTo(
+  //     ".phone",
+  //     {
+  //       translateY: 70,
+  //     },
+  //     {
+  //       translateY: -150,
+  //       scrollTrigger: {
+  //         trigger: ".phone",
+  //         scrub: true,
+  //         start: "top 80%",
+  //         ease: Power4.easeOut,
+  //       },
+  //     }
+  // );
 
-  gsap.fromTo(
-      ".back",
-      {
-        rotate: 0
-      },
-      {
-        rotate: -25,
-        scrollTrigger: {
-          trigger: ".phone",
-          scrub: true,
-          start: "top 50%",
-          end: "top 10%",
-          ease: Power4.easeOut,
-        },
-      }
-  );
-
-
-
-  gsap.fromTo(
-    ".shade",
-    {
-      backgroundImage: "linear-gradient(45deg, rgba(0, 0, 0, 0) 9%, #000 15%)",
-    },
-    {
-      backgroundImage: "linear-gradient(45deg, rgba(0, 0, 0, 0) 9%, #000 109%)",
-      scrollTrigger: {
-        trigger: ".shade",
-        scrub: true,
-        end: "bottom 20%",
-      },
-    }
-  );
-
-  gsap.fromTo(
-    ".section-5g .screen",
-    {
-      translateY: 0,
-    },
-    {
-      translateY: -250,
-      scrollTrigger: {
-        trigger: ".section-5g .screen",
-        scrub: true,
-        start: "top 50%",
-        end: "top -50%",
-      },
-    }
-  );
+  // gsap.fromTo(
+  //     ".back",
+  //     {
+  //       rotate: 0
+  //     },
+  //     {
+  //       rotate: -25,
+  //       scrollTrigger: {
+  //         trigger: ".phone",
+  //         scrub: true,
+  //         start: "top 50%",
+  //         end: "top 10%",
+  //         ease: Power4.easeOut,
+  //       },
+  //     }
+  // );
 
 
-  const items = gsap.utils.toArray(".download");
 
-  items.forEach((item) => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: item,
-        scrub: true,
-        end: "top 30%",
-      },
-    });
+  // gsap.fromTo(
+  //   ".shade",
+  //   {
+  //     backgroundImage: "linear-gradient(45deg, rgba(0, 0, 0, 0) 9%, #000 15%)",
+  //   },
+  //   {
+  //     backgroundImage: "linear-gradient(45deg, rgba(0, 0, 0, 0) 9%, #000 109%)",
+  //     scrollTrigger: {
+  //       trigger: ".shade",
+  //       scrub: true,
+  //       end: "bottom 20%",
+  //     },
+  //   }
+  // );
+  //
+  // gsap.fromTo(
+  //   ".section-5g .screen",
+  //   {
+  //     translateY: 0,
+  //   },
+  //   {
+  //     translateY: -250,
+  //     scrollTrigger: {
+  //       trigger: ".section-5g .screen",
+  //       scrub: true,
+  //       start: "top 50%",
+  //       end: "top -50%",
+  //     },
+  //   }
+  // );
 
-    tl.addLabel("start")
-      .to(item, {
-        scaleX: 1,
-        scaleY: 1,
-        duration: 1,
-      })
 
-      .to(
-        item,
-        {
-          translateY: 0,
-          duration: 1,
-        },
-        "start+=.5"
-      )
+  // const items = gsap.utils.toArray(".download");
+  //
+  // items.forEach((item) => {
+  //   let tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: item,
+  //       scrub: true,
+  //       end: "top 30%",
+  //     },
+  //   });
+  //
+  //   tl.addLabel("start")
+  //     .to(item, {
+  //       scaleX: 1,
+  //       scaleY: 1,
+  //       duration: 1,
+  //     })
+  //
+  //     .to(
+  //       item,
+  //       {
+  //         translateY: 0,
+  //         duration: 1,
+  //       },
+  //       "start+=.5"
+  //     )
+  //
+  //     .to(
+  //       item,
+  //       {
+  //         opacity: 1,
+  //         duration: 1.5,
+  //       },
+  //       "start"
+  //     );
+  // });
 
-      .to(
-        item,
-        {
-          opacity: 1,
-          duration: 1.5,
-        },
-        "start"
-      );
-  });
-
-  const fadeInElements = gsap.utils.toArray(".fade-in");
-  fadeInElements.forEach((element) => {
-    let tl_t = gsap.timeline({
-      scrollTrigger: {
-        scrub: true,
-        trigger: element,
-        start: "center 80%",
-      },
-    });
-
-    tl_t.from(element, {
-      opacity: 0,
-    });
-  });
+  // const fadeInElements = gsap.utils.toArray(".fade-in");
+  // fadeInElements.forEach((element) => {
+  //   let tl_t = gsap.timeline({
+  //     scrollTrigger: {
+  //       scrub: true,
+  //       trigger: element,
+  //       start: "center 80%",
+  //     },
+  //   });
+  //
+  //   tl_t.from(element, {
+  //     opacity: 0,
+  //   });
+  // });
 }
--- animationSections/videoSection.js
+++ animationSections/videoSection.js
@@ -1,15 +1,15 @@
 function initVideoSection() {
-  const slides = gsap.utils.toArray(".player__slide");
-  const positionModif = 100 / slides.length;
-  slides.forEach((slide, i) => {
-    slide.style.top = -i * positionModif + "%";
-    //   ScrollTrigger.create({
-    //     trigger: panel,
-    //     start: "top top",
-    //     pin: ".test",
-    //     pinSpacing: false,
-    //   });
-  });
+  // const slides = gsap.utils.toArray(".player__slide");
+  // const positionModif = 100 / slides.length;
+  // slides.forEach((slide, i) => {
+  //   slide.style.top = -i * positionModif + "%";
+  //   //   ScrollTrigger.create({
+  //   //     trigger: panel,
+  //   //     start: "top top",
+  //   //     pin: ".test",
+  //   //     pinSpacing: false,
+  //   //   });
+  // });
 
     var tl = gsap.timeline(),
         mySplitText = new SplitText("h1", {type:"words,chars"}),
@@ -25,130 +25,130 @@ function initVideoSection() {
     window.addEventListener("resize", resize);
     resize();
 
-  let tl_v = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".player__slides",
-      pin: true, // pin the trigger element while active
-      start: "top top", // when the top of the trigger hits the top of the viewport
-      end: "+=4500px",
-      scrub: true,
-      onUpdate: videoAnimationUpdated,
-    },
-  });
-
-  tl_v
-    .addLabel("start")
-    .to(".player__heading", {
-      y: -350,
-      opacity: 0,
-      duration: 5,
-      delay: 3,
-    })
-    .to(
-      ".player-wrapper__player",
-      {
-        opacity: 1,
-        duration: 5,
-      },
-      "start"
-    )
-    .addLabel("infoSlide")
-    .from(".player__text", {
-      opacity: 0,
-      y: 350,
-      duration: 5,
-    })
-    .to(
-      ".player-wrapper__player",
-      {
-        opacity: 0.1,
-        duration: 5,
-      },
-      "infoSlide"
-    )
-    .addLabel("clearVideo")
-    .to(".player__text", {
-      opacity: 0,
-      y: -350,
-      duration: 5,
-      delay: 3,
-    })
-    .to(
-      ".player-wrapper__player",
-      {
-        opacity: 1,
-        duration: 5,
-      },
-      "clearVideo"
-    )
-    .addLabel("revealVideo")
-    .to(".player-wrapper", {
-      scaleX: 0.43,
-      scaleY: 0.43,
-      duration: 10,
-    })
-
-    .to(
-      ".player-wrapper__ui",
-      {
-        opacity: 1,
-        duration: 10,
-      },
-      "revealVideo"
-    )
-    .to(
-      ".player-wrapper__hardware",
-      {
-        opacity: 1,
-        duration: 10,
-      },
-      "revealVideo"
-    )
-    .addLabel("playerExit", "+=1.5")
-    .to(".player-wrapper", {
-      y: "-=200%",
-      duration: 10,
-    })
-    .from(
-      ".player__footer",
-      {
-        opacity: 0,
-        duration: 2,
-      },
-      "playerExit"
-    )
-    .from(
-      ".player__footer .subsection-intro, .player__footer .innovation-callout",
-      {
-        opacity: 0,
-        y: 20,
-        duration: 7,
-      },
-      "playerExit"
-    )
-    .from(
-      ".section-video .innovation-callout-keyline",
-      {
-        scaleX: 0.3,
-        duration: 4,
-      },
-      "playerExit"
-    );
-
-
-  const video = document.querySelector(".player-wrapper__player");
-  video.loop = true;
-  video.muted = true; // without this line it's not working although I have "muted" in HTML
-
-  let isRuning = false;
-  function videoAnimationUpdated(event) {
-    if (!isRuning && event.progress > 0.01) {
-      video.play();
-      isRuning = true;
-    }
-    if (isRuning && (event.progress > 0.54 || event.progress < 0.1)) {
-      video.pause();
-      isRuning = false;
-    }
-  }
+  // let tl_v = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: ".player__slides",
+  //     pin: true, // pin the trigger element while active
+  //     start: "top top", // when the top of the trigger hits the top of the viewport
+  //     end: "+=4500px",
+  //     scrub: true,
+  //     onUpdate: videoAnimationUpdated,
+  //   },
+  // });
+  //
+  // tl_v
+  //   .addLabel("start")
+  //   .to(".player__heading", {
+  //     y: -350,
+  //     opacity: 0,
+  //     duration: 5,
+  //     delay: 3,
+  //   })
+  //   .to(
+  //     ".player-wrapper__player",
+  //     {
+  //       opacity: 1,
+  //       duration: 5,
+  //     },
+  //     "start"
+  //   )
+  //   .addLabel("infoSlide")
+  //   .from(".player__text", {
+  //     opacity: 0,
+  //     y: 350,
+  //     duration: 5,
+  //   })
+  //   .to(
+  //     ".player-wrapper__player",
+  //     {
+  //       opacity: 0.1,
+  //       duration: 5,
+  //     },
+  //     "infoSlide"
+  //   )
+  //   .addLabel("clearVideo")
+  //   .to(".player__text", {
+  //     opacity: 0,
+  //     y: -350,
+  //     duration: 5,
+  //     delay: 3,
+  //   })
+  //   .to(
+  //     ".player-wrapper__player",
+  //     {
+  //       opacity: 1,
+  //       duration: 5,
+  //     },
+  //     "clearVideo"
+  //   )
+  //   .addLabel("revealVideo")
+  //   .to(".player-wrapper", {
+  //     scaleX: 0.43,
+  //     scaleY: 0.43,
+  //     duration: 10,
+  //   })
+  //
+  //   .to(
+  //     ".player-wrapper__ui",
+  //     {
+  //       opacity: 1,
+  //       duration: 10,
+  //     },
+  //     "revealVideo"
+  //   )
+  //   .to(
+  //     ".player-wrapper__hardware",
+  //     {
+  //       opacity: 1,
+  //       duration: 10,
+  //     },
+  //     "revealVideo"
+  //   )
+  //   .addLabel("playerExit", "+=1.5")
+  //   .to(".player-wrapper", {
+  //     y: "-=200%",
+  //     duration: 10,
+  //   })
+  //   .from(
+  //     ".player__footer",
+  //     {
+  //       opacity: 0,
+  //       duration: 2,
+  //     },
+  //     "playerExit"
+  //   )
+  //   .from(
+  //     ".player__footer .subsection-intro, .player__footer .innovation-callout",
+  //     {
+  //       opacity: 0,
+  //       y: 20,
+  //       duration: 7,
+  //     },
+  //     "playerExit"
+  //   )
+  //   .from(
+  //     ".section-video .innovation-callout-keyline",
+  //     {
+  //       scaleX: 0.3,
+  //       duration: 4,
+  //     },
+  //     "playerExit"
+  //   );
+  //
+  //
+  // const video = document.querySelector(".player-wrapper__player");
+  // video.loop = true;
+  // video.muted = true; // without this line it's not working although I have "muted" in HTML
+  //
+  // let isRuning = false;
+  // function videoAnimationUpdated(event) {
+  //   if (!isRuning && event.progress > 0.01) {
+  //     video.play();
+  //     isRuning = true;
+  //   }
+  //   if (isRuning && (event.progress > 0.54 || event.progress < 0.1)) {
+  //     video.pause();
+  //     isRuning = false;
+  //   }
+  // }
 }
