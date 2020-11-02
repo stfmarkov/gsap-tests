9b1d6b4a61c73e5684a5e1a49089f8c456eebb34
stfmarkov <stf.markov@gmail.com>
2020-11-02 14:42:19 +0200

    icons section

--- animationSections/phoneLockup.js
+++ animationSections/phoneLockup.js
@@ -1,5 +1,13 @@
 function phoneLockup() {
-  let tl_icons = gsap.timeline({
+  //   // pin the section
+  //   ScrollTrigger.create({
+  //     trigger: ".player__slides",
+  //     start: "top top",
+  //     end: `+=${animationLength}px`,
+  //     pin: true,
+  //   });
+
+  let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".spread-icons",
       pin: ".spread-icons", // pin the trigger element while active
@@ -9,8 +17,27 @@ function phoneLockup() {
     },
   });
 
-  tl_icons.from(".spread-icons .phone-lockup-container", {
-    scaleX: 1.1,
-    scaleY: 1.1,
-  });
+  tl.addLabel("start")
+    .from(".spread-icons .center-phone .hardware-container", {
+      scale: 9.6,
+      duration: 1,
+    })
+    .from(
+      ".side-phone-left figure",
+      {
+        scale: 9.6,
+        x: -2400,
+        duration: 1,
+      },
+      "start+=0.2"
+    )
+    .from(
+      ".side-phone-right figure",
+      {
+        scale: 9.6,
+        x: 2400,
+        duration: 1,
+      },
+      "start+=0.2"
+    );
 }
