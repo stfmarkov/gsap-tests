6b20e1d2b3c62aba0ef1394deaba710ddfc1b811
stfmarkov <stf.markov@gmail.com>
2020-11-09 14:10:21 +0200

    second video

--- animationSections/logoSpin.js
+++ animationSections/logoSpin.js
@@ -5,8 +5,12 @@ function logoSpin() {
   const video = document.querySelector(".logo-spin");
   video.loop = true;
   video.muted = true; // without this line it's not working although I have "muted" in HTML
-  let isRuning = false;
   let current = 0;
+
+  const videoCoin = document.querySelector(".logo-spin-coin");
+  videoCoin.loop = true;
+  videoCoin.muted = true; // without this line it's not working although I have "muted" in HTML
+  let currentCoin = 0;
 
   // pin the section
   ScrollTrigger.create({
@@ -17,13 +21,29 @@ function logoSpin() {
     onUpdate: rotate,
   });
 
+  // pin the section
+  ScrollTrigger.create({
+    trigger: ".logo-spin-coin",
+    start: "top top",
+    end: `+=${animationLength}px`,
+    pin: true,
+    onUpdate: rotateCoin,
+  });
+
   function rotate(event) {
     const progress = event.progress;
     if (progress <= 0) return;
     current = video.duration * progress;
   }
 
+  function rotateCoin(event) {
+    const progress = event.progress;
+    if (progress <= 0) return;
+    currentCoin = videoCoin.duration * progress;
+  }
+
   setInterval(() => {
     video.currentTime = current || 0;
+    videoCoin.currentTime = currentCoin || 0;
   }, 33.33);
 }
