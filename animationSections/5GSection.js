715c7f32f6af22a0a0a159d6b7992335fb63d3cb
stefankostov <stefankostov91@gmail.com>
2020-11-10 21:17:22 +0200

    fff

--- animationSections/5GSection.js
+++ animationSections/5GSection.js
@@ -1,4 +1,79 @@
 function init5GSection() {
+
+
+    if (!document.querySelector(".loader")) return;
+
+    let tl = gsap.timeline({ totalDuration: 20 });
+
+    tl.addLabel("start")
+        .to(
+            ".loader__wrapper img",
+            {
+                opacity: 1,
+                duration: 0.7,
+                ease: Power2.easeIn,
+            },
+            "start+=1.25"
+        )
+        .to(
+            ".loader .shape1",
+            {
+                // opacity: 0,
+                strokeDashoffset: 0,
+                duration: 1.5,
+                ease: Power2.easeIn,
+            },
+            "start"
+        )
+        .to(
+            ".loader .shape0",
+            {
+                //   opacity: 0,
+                strokeDashoffset: 0,
+                duration: 1.3,
+                ease: Power2.easeIn,
+            },
+            "start+=0.15"
+        )
+        .addLabel("exit")
+        .to(".loader__wrapper", {
+            y: "-100%",
+            duration: 0.7,
+            ease: Power4.easeIn,
+        })
+        .to(
+            ".loader__base",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.13"
+        )
+        .to(
+            ".overlay",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.26"
+        )
+        .to(
+            ".last",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.39"
+        )
+        .call(() => {
+            document.querySelector(".loader").classList.add("loader__done");
+        });
+
+
+
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     };
