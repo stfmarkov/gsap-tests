9b3af9a5cc41024ce8c3522979ab1ed321e806b8
stefankostov <stefankostov91@gmail.com>
2020-11-10 23:29:00 +0200

    fff

--- animationSections/loader.js
+++ animationSections/loader.js
@@ -1,72 +1,72 @@
 function initLoader() {
-  if (!document.querySelector(".loader")) return;
+    tl.addLabel("start")
+        .to(".loader__wrapper img", {scaleX: 1.5,scaleY: 1.5, duration: 1,}, "start+=0.7")
+        .to(".loader__wrapper svg", {scaleX: 1.5,scaleY: 1.5, duration: 1,}, "start+=0.7")
+        .to(
+            ".loader__wrapper img",
+            {
+                opacity: 1,
+                duration: 0.7,
+                ease: Power2.easeIn,
+            },
+            "start+=1.25"
+        )
+        .to(
+            ".loader .shape1",
+            {
+                // opacity: 0,
+                strokeDashoffset: 0,
+                duration: 1.5,
+                ease: Power2.easeIn,
+            },
+            "start"
+        )
+        .to(
+            ".loader .shape0",
+            {
+                //   opacity: 0,
+                strokeDashoffset: 0,
+                duration: 1.3,
+                ease: Power2.easeIn,
+            },
+            "start+=0.15"
+        )
+        .addLabel("exit")
+        .to(".loader__wrapper", {
+            y: "-100%",
+            duration: 0.7,
+            ease: Power4.easeIn,
+        })
+        .to(
+            ".loader__base",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.13"
+        )
+        .to(
+            ".overlay",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.26"
+        )
+        .to(
+            ".last",
+            {
+                y: "-100%",
+                duration: 0.7,
+                ease: Power4.easeIn,
+            },
+            "exit+=0.39"
+        )
+        .call(() => {
+            document.querySelector(".loader").classList.add("loader__done");
+        });
 
-  let tl = gsap.timeline({ totalDuration: 20 });
-
-  tl.addLabel("start")
-    .to(
-      ".loader__wrapper img",
-      {
-        opacity: 1,
-        duration: 0.7,
-          ease: Power2.easeIn,
-      },
-      "start+=1.25"
-    )
-    .to(
-      ".loader .shape1",
-      {
-        // opacity: 0,
-        strokeDashoffset: 0,
-        duration: 1.5,
-          ease: Power2.easeIn,
-      },
-      "start"
-    )
-    .to(
-      ".loader .shape0",
-      {
-        //   opacity: 0,
-        strokeDashoffset: 0,
-        duration: 1.3,
-          ease: Power2.easeIn,
-      },
-      "start+=0.15"
-    )
-    .addLabel("exit")
-    .to(".loader__wrapper", {
-      y: "-100%",
-      duration: 0.7,
-        ease: Power4.easeIn,
-    })
-    .to(
-      ".loader__base",
-      {
-        y: "-100%",
-        duration: 0.7,
-          ease: Power4.easeIn,
-      },
-      "exit+=0.13"
-    )
-      .to(
-          ".overlay",
-          {
-              y: "-100%",
-              duration: 0.7,
-              ease: Power4.easeIn,
-          },
-          "exit+=0.26"
-      )
-      .to(
-          ".last",
-          {
-              y: "-100%",
-              duration: 0.7,
-              ease: Power4.easeIn,
-          },
-          "exit+=0.39"
-      )
-    .call(() => {
-      document.querySelector(".loader").classList.add("loader__done");
-    });
+    
 }
